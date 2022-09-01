3985c42d484f39b09f68c5726dc847c2801b3219
Art Ahmetaj <artahmetaj@yahoo.com>
2022-09-01 22:17:01 +0200

    Adds access token on params on campaign instance call

--- facebook-ads-visualizer/src/services/client/campaigns/campain-axios-instance.ts
+++ facebook-ads-visualizer/src/services/client/campaigns/campain-axios-instance.ts
@@ -1,8 +1,19 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 
 const facebookCampaignInstance = axios.create({
   timeout: 10000,
   baseURL: "https://graph.facebook.com",
 });
+
+facebookCampaignInstance.interceptors.request.use(
+  async (config: AxiosRequestConfig) => {
+    config.params["access_token"] = process.env.FACEBOOK_ACCESS_TOKEN;
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
 
 export default facebookCampaignInstance;
