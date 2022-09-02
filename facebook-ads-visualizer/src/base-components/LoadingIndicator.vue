88cfe1f8843e9d77e269bfe9d2d03122e8facd7c
Art Ahmetaj <artahmetaj@yahoo.com>
2022-09-02 10:39:34 +0200

    Adds a basic campaign chart and an alternative chart for other cases

---
+++ facebook-ads-visualizer/src/base-components/LoadingIndicator.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-progress-circular indeterminate :size="this.size" color="primary" />
+</template>
+
+<script lang="ts">
+// vue plugins
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "LoadingIndicator",
+  props: {
+    size: {
+      type: Number,
+      required: true,
+    },
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ facebook-ads-visualizer/src/views/home/CpcCampaignChart.vue
@@ -0,0 +1,45 @@
+<template>
+  <LineChart title-input="CPC on all campaigns per week" :chart-data="this.getChartData"></LineChart>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import LineChart from "@/base-components/charts/line-chart";
+import { ChartData } from "chart.js";
+
+export default Vue.extend({
+  name: "CpcCampaignChart",
+  components: {
+    LineChart
+  },
+
+  props: {
+    xAxis: {
+      type: Object as () => string[],
+      required: true
+    },
+    yAxis: {
+      type: Object as () => number[],
+      required: true
+    }
+  },
+  computed: {
+    getChartData(): ChartData {
+      return {
+        labels: this.xAxis,
+
+        datasets: [
+          {
+            fill: false,
+            label: "",
+            borderColor: "red",
+            data: this.yAxis
+          }
+        ]
+      };
+    }
+  }
+});
+</script>
+
+<style scoped lang="scss"></style>
